78afda74910977700e63d7f48f4d2cf5c6c0f49e
TommyZakh <tommyzakh@gmail.com>
2017-09-27 20:57:27 -0700

    More adjustments to scene, and significant tweaks to player movement

--- Assets/Standard Assets/2D/Scripts/Restarter.cs
+++ Assets/Standard Assets/2D/Scripts/Restarter.cs
@@ -6,11 +6,15 @@ namespace UnityStandardAssets._2D
 {
     public class Restarter : MonoBehaviour
     {
+        [SerializeField]
+        private Vector3 respawnPos;
+
         private void OnTriggerEnter2D(Collider2D other)
         {
             if (other.tag == "Player")
             {
-                SceneManager.LoadScene(SceneManager.GetSceneAt(0).name);
+                other.transform.position = respawnPos;
+                //SceneManager.LoadScene(SceneManager.GetSceneAt(0).name);
             }
         }
     }
